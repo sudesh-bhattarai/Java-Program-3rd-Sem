c551737cc6468a265ff7d0459980179b47eae9fd
sudesh-bhattarai <bhattaraisudesh08@gmail.com>
2025-12-17 22:47:56 +0545

    Inheritance

---
+++ HierarchialInheritance.java
@@ -0,0 +1,28 @@
+class Animal{
+    void eat(){
+        System.out.println("Animals eats food");
+    }
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Dog barks");
+    }
+}
+class Cat extends Animal{
+    void meow(){
+        System.out.println("Cat meows");
+    }
+}
+public class HierarchialInheritance {
+    public static void main(String[] args) {
+        {
+            Dog d = new Dog();
+            d.bark();
+            d.eat();
+            
+            Cat c = new Cat();
+            c.meow();
+            c.eat();
+        }
+    }
+}
