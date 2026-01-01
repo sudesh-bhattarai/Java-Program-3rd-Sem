f08f932e189a635e8c29913d240c25301d0cb44a
sudesh-bhattarai <bhattaraisudesh08@gmail.com>
2026-01-01 22:18:53 +0545

    Abstract class

---
+++ oops/AbstractTest.java
@@ -0,0 +1,32 @@
+abstract class Animal {
+    String name;
+    Animal(String name) {
+        this.name = name;
+    }
+    abstract void sound();
+    void sleep() {
+        System.out.println(name + " is sleeping");
+    }
+}
+interface Pet {
+    void play();
+}
+class Dog extends Animal implements Pet {
+    Dog(String name) {
+        super(name);
+    }
+    void sound() {
+        System.out.println(name + "Says Woof");
+    }
+    public void play() {
+        System.out.println(name + " Loves to play fetch");
+    }
+}
+public class AbstractTest {
+    public static void main(String[] args) {
+        Dog d = new Dog("Buddy");
+        d.sound();
+        d.sleep();
+        d.play();
+    }
+}
