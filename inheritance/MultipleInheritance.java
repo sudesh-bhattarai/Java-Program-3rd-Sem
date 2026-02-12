7db6014a01e019080dd41373a264921eb2b41c91
sudesh-bhattarai <bhattaraisudesh08@gmail.com>
2026-02-12 23:12:56 +0545

    Inheritance

--- inheritance/MultipleInheritance.java
+++ inheritance/MultipleInheritance.java
@@ -1,23 +1,22 @@
 //Java does not support multiple inheritance with classes to avoid complexity and ambiguity. so we use Interfaces to achieve it.
-
-interface A {
-    void methodA();
+interface A1 {
+    void methodA1();
 }
-interface B {
-    void methodB();
+interface B1 {
+    void methodB1();
 }
-class C implements A, B {
-    public void methodA(){
+class C1 implements A1, B1 {
+    public void methodA1(){
         System.out.println("Method A from Interface A");
     }
-    public void methodB(){
+    public void methodB1(){
         System.out.println("Method B from Interface B");
     }
 }
 public class MultipleInheritance {
     public static void main(String [] args){
-        C obj = new C();
-        obj.methodA();
-        obj.methodB();
+        C1 obj = new C1();
+        obj.methodA1();
+        obj.methodB1();
     }
 }
--- inheritance/SingleInheritance.java
+++ inheritance/SingleInheritance.java
@@ -1,17 +1,16 @@
-
-class Animal{
+class Animal1{
     void eat(){
         System.out.println("Animal eats food");
     }
 }
-class Dog extends Animal{
+class Dog1 extends Animal1{
     void bark(){
     System.out.println("Dog barks");
 }
 }
 public class SingleInheritance {
     public static void main(String[] args) {
-    Dog d = new Dog();
+    Dog1 d = new Dog1();
     d.bark();
     d.eat();
 }
