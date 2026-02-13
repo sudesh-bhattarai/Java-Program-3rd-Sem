cb3a449b0bde2cf236d66047cc1cef390f3f2758
sudesh-bhattarai <bhattaraisudesh08@gmail.com>
2026-02-13 23:14:43 +0545

    Upcasting

--- oops/Upcasting.java
+++ oops/Upcasting.java
@@ -1,17 +1,17 @@
-class Animal {
+class Animal2 {
     void Sound() {
         System.out.println("Animal makes sound");
     }
 }
-class Dog extends Animal {
+class Dog2 extends Animal2 {
     void bark() {
         System.out.println("Dog barks");
     }
 }
 public class Upcasting {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        Animal animal = dog;
+        Dog2 dog = new Dog2();
+        Animal2 animal = dog;
         animal.Sound();
        // animal.bark();  // This line would cause a compile-time error. we cannot call bark() method using animal reference.
     }
