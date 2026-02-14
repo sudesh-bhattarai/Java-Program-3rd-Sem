641f5736853a5ca170225b81e69cdc369bb36880
sudesh-bhattarai <bhattaraisudesh08@gmail.com>
2026-02-14 22:36:00 +0545

    Downcasting

--- oops/Downcasting.java
+++ oops/Downcasting.java
@@ -1,17 +1,17 @@
-class Animal {
+class Animal3 {
     void Sound() {
         System.out.println("Animal makes sound");
     }
 }
-class Dog extends Animal {
+class Dog3 extends Animal3 {
     void bark() {
         System.out.println("Dog barks");
     }
 }
 public class Downcasting {
     public static void main(String[] args) {
-        Animal animal = new Dog(); // Upcasting
-        Dog dog = (Dog) animal; // Downcasting
+        Animal3 animal = new Dog3(); // Upcasting
+        Dog3 dog = (Dog3) animal; // Downcasting
         dog.Sound();
         dog.bark();
     }}
